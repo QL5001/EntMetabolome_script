e974f8d3e009154d3db0ca3fb5ba6c944663ef4c
QL5001 <45108650+QL5001@users.noreply.github.com>
2023-07-02 22:00:14 -0500

    Update FullScan_LogisticRegression.py

--- FullScan_LogisticRegression.py
+++ FullScan_LogisticRegression.py
@@ -1,4 +1,4 @@
-#Logistic regression classification between exometabolomes of the four group of samples using principal component-1 (PC1) values from the sparse principal component analysis (sPCA) 
+#Logistic regression classification of exometabolomes between four group of samples using the principal component-1 (PC1) values from sparse principal component analysis (sPCA) 
 #Begin#
 
 import pandas as pd
