211622ab49b3fbfcb19d0e15fa1d38555267fe55
QL5001 <45108650+QL5001@users.noreply.github.com>
2023-07-02 21:59:44 -0500

    Update FullScan_LogisticRegression_ROC.py

--- FullScan_LogisticRegression_ROC.py
+++ FullScan_LogisticRegression_ROC.py
@@ -1,1 +1,3 @@
+#Receiver operating characteristic (ROC) curve of Logistic regression classification of exometabolomes between four groups of samples using the principal component-1 (PC1) values from parse principal component analysis (sPCA) 
 
+#Logistic regression classification between exometabolomes of the four group of samples using principal component-1 (PC1) values from the sparse principal component analysis (sPCA) 
